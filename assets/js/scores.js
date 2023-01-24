48f79d606fab8ed1d7746073d7700d93e8009f74
wagnerona <wagnerolarco@gmail.com>
2023-01-24 15:30:51 +0000

    highscore works, however when empty gives us console error

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,9 +1,16 @@
 let highScores = document.querySelector("#highscores");
+let clearBtn = document.querySelector("#clear");
 
 let lastUser = JSON.parse(localStorage.getItem("user"));
-// for (let i = 0; i < lastUser.length; i++) {
+console.log(lastUser)
+
 let highScore = document.createElement("li")
-highScore.innerText = lastUser[0];
+highScore.innerText = `${lastUser.initials} - ${lastUser.userScore}/5` //template literals to display values of object (stack overflow)
 highScores.appendChild(highScore);
 
-// }
+
+
+clearBtn.addEventListener("click", function () {
+    localStorage.clear();
+    highScores.remove();
+})
